add284f51af73cc7bab073be8a74e87e8871e784
dbrarciaga <120127890+dbrarciaga@users.noreply.github.com>
2024-01-16 08:01:19 +0800

    update 1/16/2024
    dynamic buttons

--- Admin/Applications.aspx.designer.cs
+++ Admin/Applications.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace MATH1.Admin
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl div1;
 
         /// <summary>
+        /// PlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder PlaceHolder1;
+
+        /// <summary>
         /// DropDownList1 control.
         /// </summary>
         /// <remarks>
